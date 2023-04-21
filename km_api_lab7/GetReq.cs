a9b9700782ac1e28f9273a94a6bb753f48d82683
Olga <seredaolga03@gmail.com>
2023-04-21 19:26:56 +0200

    lab7

---
+++ km_api_lab7/GetReq.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace km_api_lab7
+{
+    public class GetReq
+    {
+        HttpWebRequest _req;
+        string _adr;
+        public string Responce { get; set; }
+
+        public GetReq(string address) {
+            _adr = address;
+        }
+
+        public void Run() {
+            _req = (HttpWebRequest)WebRequest.Create(_adr);
+            _req.Method = "Get";
+
+            try {
+                HttpWebResponse response = (HttpWebResponse)_req.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null) { Responce = new StreamReader(stream).ReadToEnd(); }
+            }
+            catch (Exception) { }
+        }
+    }
+}
---
+++ km_api_lab7/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace km_api_lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"(♥назва пишеться лiтерами латинського алфавiту♥)" +
+                $"\nВведiть назву країни та натиснiть Enter");
+
+            var country = "";
+            Console.Write("\t> ");
+            country = Console.ReadLine();
+
+            var request = new GetReq($"https://restcountries.com/v3.1/name/{country}");
+            request.Run();
+
+            var response = request.Responce;
+            if (response != null) {
+                Console.WriteLine("\n   ______Результат______\n");
+                JArray j_arr = JArray.Parse(response);
+                JObject j_obj = j_arr[0].ToObject<JObject>();
+                string country_name = j_obj["name"]["official"].ToString();
+                string country_capital = j_obj["capital"][0].ToString();
+                string country_region = j_obj["region"].ToString();
+                Console.WriteLine($"   Країнa: {country_name}");
+                Console.WriteLine($"   Столиця: {country_capital}");
+                Console.WriteLine($"   Регiон: {country_region}");
+                Console.WriteLine("   _____________________\n");
+            }
+            else { Console.WriteLine("\nВ назвi країни помилка або такої країни не iснує!"); }
+            
+
+        }
+    }
+}
